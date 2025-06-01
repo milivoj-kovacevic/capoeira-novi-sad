6830cdb57a750d386005a2be41a33ef5812277f3
Milivoj Kovacevic <milivoj.kovacevic.work@gmail.com>
2025-06-01 12:28:56 +0200

    Add basic layout for website using Astro.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
