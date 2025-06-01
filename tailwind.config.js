0971c1c4715efda991a3f0799ceebbe1a16c1309
Milivoj Kovacevic <milivoj.kovacevic.work@gmail.com>
2025-06-01 12:58:15 +0200

    Update the theme of the site and add dark mode toggle for testing.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}",
     "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cas-blue': '#232b5a',
+        'cas-yellow': '#ffe600',
+        'cas-green': '#1db954',
+      },
+    },
   },
   plugins: [],
 }
