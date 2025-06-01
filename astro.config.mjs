b408f693105c4ee1d20e4ba6a311c0bdd9b5b46d
Milivoj Kovacevic <milivoj.kovacevic.work@gmail.com>
2025-06-01 13:16:46 +0200

    Update Astro config to fix deployment to pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   integrations: [tailwind()],
+  base: isProd ? '/capoeira-novi-sad/' : '/',
 });
