671280f94ffd3ffbf4aa52bfbaf51005e9f25c54
Milivoj Kovacevic <milivoj.kovacevic.work@gmail.com>
2025-06-12 21:40:06 +0200

    Revert "Update config for prod."
    This reverts commit f4f084578384f557ab47d1a98bc329d528c6964a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,17 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import i18next from "astro-i18next";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import i18next from 'astro-i18next';
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  // only set `site` in production
-  ...(isProd && {
-    site: "https://milivojk13.github.io/capoeira-novi-sad/",
-    base: "/capoeira-novi-sad/",
-  }),
-
-  // in dev, omit `site` (Astro will generate relative URLs)
-  // you can still set a different base for dev if you like:
-  ...(!isProd && {
-    base: "/",
-  }),
   integrations: [
     tailwind(),
     i18next({
-      defaultLocale: "sr", // <-- Force Serbian as default
-      base: isProd ? '/capoeira-novi-sad/' : '/',
+      defaultLocale: 'sr', // <-- Force Serbian as default
+      // base: isProd ? '/capoeira-novi-sad/' : '/',
       // ...other options
     }),
   ],
+  // base: isProd ? '/capoeira-novi-sad/' : '/',
 });
